fd8913155a52b50bb1058c86386ef4e8e5c33465
Hari sainath <harisainath51@gmail.com>
2023-01-20 15:51:42 +0530

    videoplayer working

--- App.js
+++ App.js
@@ -1,10 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar,setStatusBarHidden } from 'expo-status-bar';
+import { Dimensions,StyleSheet, NativeModules, TouchableOpacity, View } from 'react-native';
+import { ResizeMode } from 'expo-av'
+import VideoPlayer from 'expo-video-player'
+import * as ScreenOrientation from 'expo-screen-orientation'
+import React, { useState,useRef } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const width=Dimensions.get('window').width;
+const height=Dimensions.get('window').height;
 export default function App() {
+  const [inFullscreen, setInFullsreen] = useState(false)
+  const [inFullscreen2, setInFullsreen2] = useState(false)
+  const refVideo2 = useRef(0)
+  const [curtime, setcurtime] = useState();
+ 
+
+ const exitScreen = async () =>{
+      setStatusBarHidden(false, 'fade')
+      setInFullsreen2(!inFullscreen2)
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
+ }
+ const forward = () =>{
+  refVideo2.current.playFromPositionAsync(+curtime.positionMillis + +10000)
+ }
+
+ const backward = () =>{
+  refVideo2.current.playFromPositionAsync(+curtime.positionMillis - +10000)
+ }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <VideoPlayer
+        videoProps={{
+          shouldPlay: true,
+          defaultControlsVisible: false,
+          resizeMode: ResizeMode.CONTAIN,
+          source: {
+            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          },
+          ref: refVideo2,
+        }}
+        
+        header={<View style={{
+          width:"100%",
+          }}>
+          
+          {inFullscreen2 ? 
+              <TouchableOpacity onPress={exitScreen}>
+                <Ionicons name="arrow-back" size={30} color="#ffffff" style={{marginTop:10}}/>
+              </TouchableOpacity> : 
+              <TouchableOpacity >
+                <Ionicons name="arrow-back" size={30} color="#ffffff" style={{marginTop:10}}/>
+              </TouchableOpacity>
+              }
+              
+              <View style={{width:'100%',flexDirection:'row'}}>
+                
+                <View style={{position:'absolute',
+                left: inFullscreen2 ? ((width/2)-20) : 50,
+                top:110}}>
+                  <TouchableOpacity onPress={backward}><Ionicons name="ios-play-back-circle" size={40} color="" style={{color:"#ffffff"}}/></TouchableOpacity>
+                </View>
+                <View style={{position:'absolute',
+                right:inFullscreen2 ? ((width/2)-20) : 50,
+                top:110}}>
+                  <TouchableOpacity onPress={forward}><Ionicons name="play-forward-circle" size={40} color="" style={{color:"#ffffff"}}/></TouchableOpacity>
+                </View>
+              </View>
+        
+        </View>}
+        fullscreen={{
+          inFullscreen: inFullscreen2,
+          enterFullscreen: async () => {
+            setStatusBarHidden(true, 'fade')
+            setInFullsreen2(!inFullscreen2)
+            await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT)
+            refVideo2.current.setStatusAsync({
+              shouldPlay: true,
+            })
+          },
+          exitFullscreen: async () => {
+            setStatusBarHidden(false, 'fade')
+            setInFullsreen2(!inFullscreen2)
+            await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
+            refVideo2.current.setStatusAsync({
+              shouldPlay: true,
+            })
+          },
+        }}
+        style={{
+          videoBackgroundColor: 'black',
+          height: inFullscreen2 ? width : 300,
+          width: inFullscreen2 ? height : width,
+        }}
+        playbackCallback={(playbackStatus) => {
+          setcurtime(playbackStatus)
+          if(inFullscreen2)
+          setInFullsreen2(inFullscreen2)
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
