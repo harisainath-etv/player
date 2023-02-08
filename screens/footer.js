790bcb8838e6b4b5dd8b4f3bb426dc4eebcd35f8
Hari sainath <harisainath51@gmail.com>
2023-02-08 12:01:30 +0530

    dynamic color with prop

--- screens/footer.js
+++ screens/footer.js
@@ -1,40 +1,78 @@
-import { View, StyleSheet,Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import React from 'react'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { PAGE_WIDTH, BACKGROUND_COLOR, IMAGE_BORDER_COLOR,NORMAL_TEXT_COLOR } from '../constants'
+import { PAGE_WIDTH, BACKGROUND_COLOR, IMAGE_BORDER_COLOR, NORMAL_TEXT_COLOR, SLIDER_PAGINATION_SELECTED_COLOR } from '../constants'
 
-export default function Footer() {
-    const [color, setColor] = useState(NORMAL_TEXT_COLOR)
+export default function Footer(props) {
+    const pageName = props.pageName;
     return (
         <View>
             <View style={styles.footerContainer}>
 
-                <View style={styles.iconContainer}>
-                    <MaterialCommunityIcons name="home" size={28} color={color} />
-                    <Text style={styles.footerText}>HOME</Text>
-                </View>
+                {pageName == 'Home' ?
 
-                <View style={styles.iconContainer}>
-                    <MaterialCommunityIcons name="television-classic" size={28} color={color} />
-                    <Text style={styles.footerText}>TV CHANNELS</Text>
-                </View>
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="home" size={28} color={SLIDER_PAGINATION_SELECTED_COLOR} />
+                        <Text style={[styles.footerText, { color: SLIDER_PAGINATION_SELECTED_COLOR }]}>HOME</Text>
+                    </View>
+                    :
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="home" size={28} color={NORMAL_TEXT_COLOR} />
+                        <Text style={styles.footerText}>HOME</Text>
+                    </View>
 
-                <View style={styles.iconContainer}>
-                    <MaterialCommunityIcons name="newspaper-variant-multiple-outline" size={28} color={color} />
-                    <Text style={styles.footerText}>NEWS</Text>
-                </View>
+                }
 
-                <View style={styles.iconContainer}>
-                    <MaterialCommunityIcons name="download" size={28} color={color} />
-                    <Text style={styles.footerText}>OFFLINE</Text>
-                </View>
+                {pageName == 'TV-CHANNELS' ?
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="television-classic" size={28} color={SLIDER_PAGINATION_SELECTED_COLOR} />
+                        <Text style={[styles.footerText, { color: SLIDER_PAGINATION_SELECTED_COLOR }]}>TV CHANNELS</Text>
+                    </View>
+                    :
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="television-classic" size={28} color={NORMAL_TEXT_COLOR} />
+                        <Text style={styles.footerText}>TV CHANNELS</Text>
+                    </View>
+                }
 
-                <View style={styles.iconContainer}>
-                    <MaterialCommunityIcons name="sticker-plus" size={28} color={color} />
-                    <Text style={styles.footerText}>WATCH LATER</Text>
-                </View>
+                {pageName == 'NEWS' ?
+
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="newspaper-variant-multiple-outline" size={28} color={SLIDER_PAGINATION_SELECTED_COLOR} />
+                        <Text style={[styles.footerText, { color: SLIDER_PAGINATION_SELECTED_COLOR }]}>NEWS</Text>
+                    </View>
+                    :
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="newspaper-variant-multiple-outline" size={28} color={NORMAL_TEXT_COLOR} />
+                        <Text style={styles.footerText}>NEWS</Text>
+                    </View>
+                }
+                {pageName == 'OFFLINE' ?
+
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="download" size={28} color={SLIDER_PAGINATION_SELECTED_COLOR} />
+                        <Text style={[styles.footerText, { color: SLIDER_PAGINATION_SELECTED_COLOR }]}>OFFLINE</Text>
+                    </View>
+                    :
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="download" size={28} color={NORMAL_TEXT_COLOR} />
+                        <Text style={styles.footerText}>OFFLINE</Text>
+                    </View>
+                }
+                {pageName == 'WATCH-LATER' ?
+
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="sticker-plus" size={28} color={SLIDER_PAGINATION_SELECTED_COLOR} />
+                        <Text style={[styles.footerText, { color: SLIDER_PAGINATION_SELECTED_COLOR }]}>WATCH LATER</Text>
+                    </View>
+                    :
+                    <View style={styles.iconContainer}>
+                        <MaterialCommunityIcons name="sticker-plus" size={28} color={NORMAL_TEXT_COLOR} />
+                        <Text style={styles.footerText}>WATCH LATER</Text>
+                    </View>
+                }
 
             </View>
             <View style={styles.chromeCast}>
@@ -45,9 +83,9 @@ export default function Footer() {
 }
 
 const styles = StyleSheet.create({
-    footerText:{color:NORMAL_TEXT_COLOR,fontSize:12},
-    footerContainer: { width: PAGE_WIDTH, backgroundColor: BACKGROUND_COLOR, height: 50, borderTopLeftRadius: 15, borderTopRightRadius: 15, flexDirection: 'row', justifyContent: 'space-between', alignItems: "center",paddingLeft:10,paddingRight:10 },
-    iconContainer:{justifyContent:'center',alignItems:'center'},
+    footerText: { color: NORMAL_TEXT_COLOR, fontSize: 12 },
+    footerContainer: { width: PAGE_WIDTH, backgroundColor: BACKGROUND_COLOR, height: 50, borderTopLeftRadius: 15, borderTopRightRadius: 15, flexDirection: 'row', justifyContent: 'space-between', alignItems: "center", paddingLeft: 10, paddingRight: 10 },
+    iconContainer: { justifyContent: 'center', alignItems: 'center' },
     chromeCast: {
         width: 56,
         height: 56,
