78e251981fad2e5c6271ec3ec6d5547d7310282f
Hari sainath <harisainath51@gmail.com>
2023-03-28 15:27:42 +0530

    video url callback fix

--- screens/episode.js
+++ screens/episode.js
@@ -26,7 +26,7 @@ export default function Episode({ navigation, route }) {
   const [contentRating, setContentRating] = useState();
   const [displayGenres, setDisplayGenres] = useState();
   const [description, setDescription] = useState();
-  const [playUrl, setPlayUrl] = useState();
+  const [playUrl, setPlayUrl] = useState("");
   const [fullscreen, setFullscreen] = useState(false);
   const [play, setPlay] = useState(true);
   const [loading, setLoading] = useState(false);
@@ -48,97 +48,99 @@ export default function Episode({ navigation, route }) {
   }
 
   const loadData = async () => {
-    setLoading(true);
-    const baseUrl = FIRETV_BASE_URL;
-    var removequeryStrings = seourl.split("?");
-    var splittedData = removequeryStrings[0].split("/");
-    splittedData = splittedData.filter(function (e) { return e });
-    const checkNews = filterItems('news', splittedData);
-    const checkShow = filterItems('show', splittedData);
-    const checkSeason = filterItems('season', splittedData);
-    const checkChannel = filterItems('channel', splittedData);
-    const checkEvent = filterItems('event', splittedData);
-    const region = await AsyncStorage.getItem('country_code');
-    var urlPath = "";
-    if (splittedData.length == 4 && checkChannel == 0) {
-      urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/subcategories/" + splittedData[2] + "/episodes/" + splittedData[3];
-    }
-    else if (splittedData[0] == 'tv-shows') {
-      // if (splittedData[3] == "" || splittedData[3] == null || splittedData[3] == 'undefined')
-      //   urlPath = baseUrl + "catalogs/" + splittedData[0] + "/episodes/" + splittedData[1];
-      // else
-      urlPath = baseUrl + "catalogs/" + splittedData[0] + "/episodes/" + splittedData[splittedData.length - 1];
-    }
-    else if (splittedData[0] == 'news' || checkNews.length > 0) {
-      urlPath = baseUrl + "catalogs/" + splittedData[splittedData.length - 3] + "/items/" + splittedData[splittedData.length - 2] + "/episodes/" + splittedData[splittedData.length - 1];
-    }
-    else if (checkShow.length > 0 || checkEvent.length > 0) {
-      urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[splittedData.length - 2] + "/episodes/" + splittedData[splittedData.length - 1];
-    }
-    else if (checkChannel.length > 0) {
-      urlPath = baseUrl + "catalogs/" + splittedData[1] + "/items/" + splittedData[splittedData.length - 1];
-    }
-    else if (theme == "videolist") {
-      urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/videolists/" + splittedData[2];
-    }
-    else {
-      //if (splittedData.length == 2)
-      urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[splittedData.length - 1];
-      // if (splittedData.length == 3)
-      //   urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/" + splittedData[2];
-      // if (splittedData.length == 4)
-      //   urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/" + splittedData[2] + "/" + splittedData[3];
-    }
-    const url = urlPath + ".gzip?&auth_token=" + AUTH_TOKEN + "&region=" + region;
-    // console.log(seourl);
-    // console.log(url);
-    await axios.get(url).then(response => {
-      setTitle(response.data.data.title);
-      if (response.data.data.hasOwnProperty('channel_object'))
-        setChannel(response.data.data.channel_object.name);
-      if (response.data.data.hasOwnProperty('cbfc_rating'))
-        setContentRating(response.data.data.cbfc_rating);
-      if (response.data.data.hasOwnProperty('display_genres'))
-        setDisplayGenres(response.data.data.display_genres.join(","));
-      if (response.data.data.hasOwnProperty('description'))
-        setDescription(response.data.data.description);
-      // setContentId(response.data.data.content_id);
-      // setCatalogId(response.data.data.catalog_id);
+    if (playUrl == "") {
+      setLoading(true);
+      const baseUrl = FIRETV_BASE_URL;
+      var removequeryStrings = seourl.split("?");
+      var splittedData = removequeryStrings[0].split("/");
+      splittedData = splittedData.filter(function (e) { return e });
+      const checkNews = filterItems('news', splittedData);
+      const checkShow = filterItems('show', splittedData);
+      const checkSeason = filterItems('season', splittedData);
+      const checkChannel = filterItems('channel', splittedData);
+      const checkEvent = filterItems('event', splittedData);
+      const region = await AsyncStorage.getItem('country_code');
+      var urlPath = "";
+      if (splittedData.length == 4 && checkChannel == 0) {
+        urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/subcategories/" + splittedData[2] + "/episodes/" + splittedData[3];
+      }
+      else if (splittedData[0] == 'tv-shows') {
+        // if (splittedData[3] == "" || splittedData[3] == null || splittedData[3] == 'undefined')
+        //   urlPath = baseUrl + "catalogs/" + splittedData[0] + "/episodes/" + splittedData[1];
+        // else
+        urlPath = baseUrl + "catalogs/" + splittedData[0] + "/episodes/" + splittedData[splittedData.length - 1];
+      }
+      else if (splittedData[0] == 'news' || checkNews.length > 0) {
+        urlPath = baseUrl + "catalogs/" + splittedData[splittedData.length - 3] + "/items/" + splittedData[splittedData.length - 2] + "/episodes/" + splittedData[splittedData.length - 1];
+      }
+      else if (checkShow.length > 0 || checkEvent.length > 0) {
+        urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[splittedData.length - 2] + "/episodes/" + splittedData[splittedData.length - 1];
+      }
+      else if (checkChannel.length > 0) {
+        urlPath = baseUrl + "catalogs/" + splittedData[1] + "/items/" + splittedData[splittedData.length - 1];
+      }
+      else if (theme == "videolist") {
+        urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/videolists/" + splittedData[2];
+      }
+      else {
+        //if (splittedData.length == 2)
+        urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[splittedData.length - 1];
+        // if (splittedData.length == 3)
+        //   urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/" + splittedData[2];
+        // if (splittedData.length == 4)
+        //   urlPath = baseUrl + "catalogs/" + splittedData[0] + "/items/" + splittedData[1] + "/" + splittedData[2] + "/" + splittedData[3];
+      }
+      const url = urlPath + ".gzip?&auth_token=" + AUTH_TOKEN + "&region=" + region;
+      // console.log(seourl);
+      // console.log(url);
+      await axios.get(url).then(response => {
+        setTitle(response.data.data.title);
+        if (response.data.data.hasOwnProperty('channel_object'))
+          setChannel(response.data.data.channel_object.name);
+        if (response.data.data.hasOwnProperty('cbfc_rating'))
+          setContentRating(response.data.data.cbfc_rating);
+        if (response.data.data.hasOwnProperty('display_genres'))
+          setDisplayGenres(response.data.data.display_genres.join(","));
+        if (response.data.data.hasOwnProperty('description'))
+          setDescription(response.data.data.description);
+        // setContentId(response.data.data.content_id);
+        // setCatalogId(response.data.data.catalog_id);
 
-      var md5String = stringMd5(response.data.data.catalog_id + response.data.data.content_id + "" + currentTimestamp + SECRET_KEY)
-      //console.log(response.data.data.content_id);
-      axios.post(FIRETV_BASE_URL + "v2/users/get_all_details", {
-        catalog_id: response.data.data.catalog_id,
-        content_id: response.data.data.content_id,
-        category: "",
-        region: region,
-        auth_token: VIDEO_AUTH_TOKEN,
-        access_token: ACCESS_TOKEN,
-        id: "",
-        ts: currentTimestamp,
-        md5: md5String
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-        }
+        var md5String = stringMd5(response.data.data.catalog_id + response.data.data.content_id + "" + currentTimestamp + SECRET_KEY)
+        //console.log(response.data.data.content_id);
+        axios.post(FIRETV_BASE_URL + "v2/users/get_all_details", {
+          catalog_id: response.data.data.catalog_id,
+          content_id: response.data.data.content_id,
+          category: "",
+          region: region,
+          auth_token: VIDEO_AUTH_TOKEN,
+          access_token: ACCESS_TOKEN,
+          id: "",
+          ts: currentTimestamp,
+          md5: md5String
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+          .then(response => {
+            if (response.data.data.stream_info.adaptive_url != "")
+              setPlayUrl(response.data.data.stream_info.adaptive_url);
+            else
+              if (response.data.data.stream_info.preview.adaptive_url != "")
+                setPlayUrl(response.data.data.stream_info.preview.adaptive_url);
+
+            setLoading(false);
+          }).catch(error => {
+            console.log(JSON.stringify(error.response.data));
+            setLoading(false);
+          }
+          )
+        setLoading(false);
+      }).catch(error => {
+        setLoading(false);
       })
-        .then(response => {
-          if (response.data.data.stream_info.adaptive_url != "")
-            setPlayUrl(response.data.data.stream_info.adaptive_url);
-          else
-            if (response.data.data.stream_info.preview.adaptive_url != "")
-              setPlayUrl(response.data.data.stream_info.preview.adaptive_url);
-
-          setLoading(false);
-        }).catch(error => {
-          console.log(JSON.stringify(error.response.data));
-          setLoading(false);
-        }
-        )
-      setLoading(false);
-    }).catch(error => {
-      setLoading(false);
-    })
+    }
   }
   useEffect(() => {
     loadData()
