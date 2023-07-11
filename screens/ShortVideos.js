8fb6c60f08fdcdcb2c1e3f1cd918e7123fefee12
Hari sainath <harisainath51@gmail.com>
2023-07-11 09:54:08 +0530

    contiinue watching bug fix, short videos bug fix

--- screens/ShortVideos.js
+++ screens/ShortVideos.js
@@ -14,7 +14,7 @@ export default function Shorts() {
     const [Videos, setVideos] = useState([]);
     const [loginid, setloginid] = useState();
     var loadedvideos = [];
-    const getData = async () => {
+    const getData = async (startindex) => {
         var loginiddetails = "";
         const email = await AsyncStorage.getItem('email_id');
         const mobile_number = await AsyncStorage.getItem('mobile_number');
@@ -42,12 +42,13 @@ export default function Shorts() {
                     var indvideo = jsonObj.data[s].shorts_url;
                     var indid = jsonObj.data[s].id;
                     var liked = jsonObj.data[s].liked;
+                    var disliked = jsonObj.data[s].disliked;
                     var totallikes = jsonObj.data[s].total_likes;
                     var totalviews = jsonObj.data[s].total_views;
-                    loadedvideos.push({ "id": indid, "video": indvideo, "liked": liked, "totallikes": totallikes, "totalviews": totalviews })
+                    var totaldislikes = jsonObj.data[s].total_dislikes;
+                    loadedvideos.push({ "id": indid, "video": indvideo, "liked": liked, "disliked": disliked, "totallikes": totallikes, "totalviews": totalviews, "totaldislikes": totaldislikes })
                 }
                 setVideos((Videos) => [...Videos, ...loadedvideos]);
-
             }).catch(err => {
                 console.log(err);
             })
@@ -56,7 +57,7 @@ export default function Shorts() {
     useEffect(() => {
         if (dataFetch.current) return;
         dataFetch.current = true;
-        getData()
+        getData(0)
     })
     const flatListRef = useRef();
     const videoRef = useRef();
@@ -66,23 +67,26 @@ export default function Shorts() {
     const [likes, setlikes] = useState(0);
     const [views, setviews] = useState(0);
     const [likedvideo, setlikedvideo] = useState(false);
+    const [dislikedvideo, setdislikedvideo] = useState(false);
     const [currentid, setcurrentid] = useState();
     const [scrollvideoid, setscrollvideoid] = useState();
     const [loading, setloading] = useState(false);
     const [totallikes, settotallikes] = useState(0);
+    const [totaldislikes, settotaldislikes] = useState(0);
     const loadcontrols = async () => {
         //setshowcontrols(!showcontrols)
     }
     useEffect(() => {
         // if (showcontrols) {
-        //     setTimeout(function () { setshowcontrols(!showcontrols) }, 5000);
+        //     setTimeout(function () { setshowcontrols(!showcontrols) }, 5000);
         // }
     })
     const onLoadStart = () => {
         setState({ opacity: 1 });
     }
-    const onLoad = () => {
+    const onLoad = (val) => {
         setState({ opacity: 0 });
+        getlikes(val)
     }
     const onBuffer = ({ isBuffering }) => {
         setState({ opacity: isBuffering ? 1 : 0 });
@@ -90,12 +94,14 @@ export default function Shorts() {
 
     const likevideo = async (videoid) => {
         if (loginid != "" && loginid != null && loginid != 'null' && loginid != 'undefined') {
-            setlikedvideo(!likedvideo);
+            console.log(likedvideo);
             setcurrentid(videoid);
             axios.post(SHORTS_BASE_URL + "Welcome/likevideo", {
                 loginid: loginid,
                 videoid: videoid
             }, { headers: {} }).then(response => {
+                console.log(response.data);
+                setlikedvideo(!likedvideo);
             }).catch(error => {
                 console.log(error);
             })
@@ -104,13 +110,31 @@ export default function Shorts() {
             alert("Please login to like the video");
         }
     }
+
+    const dislikevideo = async (videoid) => {
+        if (loginid != "" && loginid != null && loginid != 'null' && loginid != 'undefined') {
+            setdislikedvideo(!dislikedvideo);
+            setcurrentid(videoid);
+            axios.post(SHORTS_BASE_URL + "Welcome/dislikevideo", {
+                loginid: loginid,
+                videoid: videoid
+            }, { headers: {} }).then(response => {
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+        else {
+            alert("Please login to dislike the video");
+        }
+    }
+
     const shareVideo = async () => {
 
     }
     function kFormatter(num) {
         return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000).toFixed(1)) + 'k' : Math.sign(num) * Math.abs(num)
     }
-    const getlikes = async(val) =>{
+    const getlikes = async (val) => {
         setloading(true);
         var videoslist = JSON.parse(JSON.stringify(Videos));
         var currentvideo = JSON.parse(JSON.stringify(videoslist[val]));
@@ -125,7 +149,17 @@ export default function Shorts() {
             else {
                 setlikedvideo(false);
             }
+
+            if (resp.data.userdislike == 1) {
+                setdislikedvideo(true);
+            }
+            else {
+                setdislikedvideo(false);
+            }
+
+
             settotallikes(resp.data.totallikes[0].total_likes)
+            settotaldislikes(resp.data.totaldislikes[0].total_dislikes)
         }).catch(err => {
             setloading(false);
             console.log(err);
@@ -147,16 +181,16 @@ export default function Shorts() {
                 onScroll={e => {
                     var val = Math.round(e.nativeEvent.contentOffset.y.toFixed(0) / PAGE_HEIGHT);
                     setcurrentIndexValue(Math.round(e.nativeEvent.contentOffset.y.toFixed(0) / PAGE_HEIGHT));
-                    if (!loadedindex.includes(val)) {
-                        loadedindex.push(val);
+                    // if (!loadedindex.includes(val)) {
+                    //     loadedindex.push(val);
                         if (val % 2 == 0 && val != 0 && val != 1) {
-                            setstartindex(startindex + limit);
-                            getData()
-                        }
-                    }
+                        setstartindex(startindex + limit);
+                        getData(startindex + limit)
+                         }
+                    //}
 
                     if (val != scrollvideoid) {
-                       getlikes(val);
+                        getlikes(val);
                     }
                 }}
                 contentContainerStyle={{ minHeight: '100%', }}
@@ -165,7 +199,7 @@ export default function Shorts() {
                         return (
 
                             <Pressable onPress={loadcontrols} style={{ width: PAGE_WIDTH, height: Math.round(PAGE_HEIGHT), flex: 1, flexGrow: 1 }}>
-                                {/* {currentIndexValue === index ? */}
+
                                 <View style={{ flex: 1, flexGrow: 1, justifyContent: 'center', alignContent: 'center' }}>
                                     <Video
                                         ref={videoRef}
@@ -173,7 +207,7 @@ export default function Shorts() {
                                         source={{ uri: item.video }}
                                         controls={true}
                                         onLoadStart={onLoadStart}
-                                        onLoad={onLoad}
+                                        onLoad={()=>onLoad(currentIndexValue)}
                                         paused={currentIndexValue === index ? false : true}
                                         playInBackground={false}
                                         repeat={true}
@@ -183,54 +217,53 @@ export default function Shorts() {
                                         style={{ width: PAGE_WIDTH, height: Math.round(PAGE_HEIGHT), flexGrow: 1, flex: 1 }}
                                         playWhenInactive={false}
                                     />
-                                    <ActivityIndicator
-                                        animating
-                                        size="large"
-                                        color={SLIDER_PAGINATION_SELECTED_COLOR}
-                                        style={[styles.activityIndicator, { opacity: state.opacity }]}
-                                    />
+                                    {/* {currentIndexValue === index ?
+                                        <ActivityIndicator
+                                            animating
+                                            size="large"
+                                            color={SLIDER_PAGINATION_SELECTED_COLOR}
+                                            style={[styles.activityIndicator, { opacity: state.opacity }]}
+                                        />
+                                        :
+                                        ""} */}
                                 </View>
-                                <View style={{ position: 'absolute', right: 15, bottom: 100, }}>
+                                <View style={{ position: 'absolute', right: 15, top: '50%', }}>
 
                                     {
                                         likedvideo == true ?
 
-                                            <Pressable onPress={() => likevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="like1" size={30} color="#ffffff" style={{}} />
-                                                {totallikes != 0 ?
-                                                    <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(totallikes)}</Text>
-                                                    :
-                                                    <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(item.totallikes)}</Text>
-                                                }
+                                            <Pressable onPress={() => likevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="like1" size={35} color={SLIDER_PAGINATION_SELECTED_COLOR} style={{}} />
+                                                {/* {totallikes != 0 ?
+                                                    <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(totallikes)}</Text>
+                                                    :
+                                                    <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(item.totallikes)}</Text>
+                                                } */}
                                             </Pressable>
 
                                             :
-
-                                            item.liked == 1 ?
-                                                <Pressable onPress={() => likevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="like1" size={30} color="#ffffff" style={{}} />
-                                                    {totallikes != 0 ?
-                                                        <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(totallikes)}</Text>
-                                                        :
-                                                        <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(item.totallikes)}</Text>
-                                                    }
-                                                </Pressable>
-                                                :
-                                                <Pressable onPress={() => likevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="like2" size={30} color="#ffffff" style={{}} />
-                                                    {totallikes != 0 ?
-                                                        <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(totallikes)}</Text>
-                                                        :
-                                                        <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(item.totallikes)}</Text>
-                                                    }
-                                                </Pressable>
+                                            <Pressable onPress={() => likevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="like2" size={35} color={SLIDER_PAGINATION_SELECTED_COLOR} style={{}} /></Pressable>
 
                                     }
 
 
-                                    <Pressable onPress={shareVideo}><AntDesign name="sharealt" size={30} color="#ffffff" style={{ marginTop: 40 }} /></Pressable>
-                                    <Pressable style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="eye" size={30} color="#ffffff" style={{ marginTop: 40 }} />
-                                        <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(item.totalviews)}</Text></Pressable>
+                                    {
+                                        dislikedvideo == true ?
+
+                                            <Pressable onPress={() => dislikevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center', marginTop: 50 }}><AntDesign name="dislike1" size={35} color={SLIDER_PAGINATION_SELECTED_COLOR} style={{}} /></Pressable>
+
+                                            :
+                                            <Pressable onPress={() => dislikevideo(item.id)} style={{ justifyContent: 'center', alignItems: 'center', marginTop: 50 }}><AntDesign name="dislike2" size={35} color={SLIDER_PAGINATION_SELECTED_COLOR} style={{}} /></Pressable>
+
+                                    }
+
+
+                                    <Pressable onPress={shareVideo}><AntDesign name="sharealt" size={35} color={SLIDER_PAGINATION_SELECTED_COLOR} style={{ marginTop: 50 }} /></Pressable>
+
+                                    {/* <Pressable style={{ justifyContent: 'center', alignItems: 'center' }}><AntDesign name="eye" size={30} color="#ffffff" style={{ marginTop: 40 }} />
+                                        <Text style={{ color: NORMAL_TEXT_COLOR, fontWeight: 'bold' }}>{kFormatter(item.totalviews)}</Text></Pressable> */}
                                 </View>
                                 {/* :
-                                    ""} */}
+                                    ""} */}
                             </Pressable>
 
                         );
--- screens/episode.js
+++ screens/episode.js
@@ -202,7 +202,7 @@ export default function Episode({ navigation, route }) {
             if (onlineplayUrl == false) {
               if (response.data.data.stream_info.adaptive_url != "") {
                 setPlayUrl(response.data.data.stream_info.adaptive_url);
-                setseektime(response.data.data.playlists.pos);
+                setseektime(response.data.data.stream_info.play_back_time);
               }
               else
                 if (response.data.data.stream_info.preview.adaptive_url != "") {
@@ -523,11 +523,10 @@ export default function Episode({ navigation, route }) {
       var sessionId = await AsyncStorage.getItem('session');
       if (sessionId != "" && sessionId != null && timestamp != "" && timestamp != null) {
         await axios.post(FIRETV_BASE_URL + "users/" + sessionId + "/playlists/watchhistory", {
-          listitem: { catalog_id: catalogId, content_id: contentId, like_count: "true" },
+          listitem: { catalog_id: catalogId, content_id: contentId, like_count: "true", play_back_status: "playing",
+          play_back_time: timestamp },
           auth_token: VIDEO_AUTH_TOKEN,
-          access_token: ACCESS_TOKEN,
-          play_back_status: "playing",
-          play_back_time: timestamp
+          access_token: ACCESS_TOKEN
         }, {
           headers: {
             'Content-Type': 'application/json',
@@ -665,9 +664,9 @@ export default function Episode({ navigation, route }) {
                 }}
                 onLoad={(data) => {
                   setDuration(data.duration)
-                  if (seektime != "" && seektime != null) {
+                  if (seektime != "" && seektime != null && data!='' && data!=null) {
                     var splittedtime = seektime.split(":");
-                    videoRef.current.seek(splittedtime[0] * 3600 + splittedtime[1] * 60 + splittedtime[2]);
+                    videoRef.current.seek(+(splittedtime[0] * 3600) + +(splittedtime[1] * 60) + +(splittedtime[2]));
                   }
 
                   GoogleCast.getCastState().then(state => {
